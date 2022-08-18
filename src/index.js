feeea231f1b438868226cfd1a65df3f9eee97479
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-08-18 22:53:28 +0300

    Testing the main function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+function App() {
+  return <div>Hello World</div>;
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
