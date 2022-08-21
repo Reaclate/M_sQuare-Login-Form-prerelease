a6f6e9b08a54d2fd0e4eb6f82beb4e47835ba90d
Tinsaye Simeneh <tinsayesimeneh608@gmail.com>
2022-08-21 21:41:27 +0300

    default email and pass added to the site UI

--- src/index.js
+++ src/index.js
@@ -42,6 +42,8 @@ export default function Login() {
   return (
     <>
       <Container className="text-center mt-4 shadow-lg p-3 mb-5 bg-white rounded">
+        Default Email is msquare@gmail.com
+        Default Password is msquarepass
         <Row>
           <Col sm="12" className="mb-3 mt-2">
             <Image src={SideImg} alt="Side" responsive />
